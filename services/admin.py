f5ed4868d5c55b14417c4b4f2f9c9377d2041c17
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-06-25 13:29:45 +0000

    Add PricingTier to admin

--- services/admin.py
+++ services/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Service, Category
+from .models import Service, Category, PricingTier
 
 # Register your models here.
 admin.site.register(Service)
 admin.site.register(Category)
+admin.site.register(PricingTier)
