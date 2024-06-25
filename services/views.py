856377264dea3afe268a5172156f9da1b70045f2
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-06-25 09:12:30 +0000

    Create + setup services app

---
+++ services/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from .models import Service
+
+
+def all_services(request):
+    """ A view to show all products, including sorting and search queries """
+
+    services = Service.objects.all()
+
+    context = {
+        'services': services,
+    }
+
+    return render(request, 'services/services.html')
