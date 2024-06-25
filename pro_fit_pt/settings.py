87913b4c7636e6633ab0a764797d858de052f5b6
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-06-25 10:02:28 +0000

    Set secrey key + DEBUG based on env

--- pro_fit_pt/settings.py
+++ pro_fit_pt/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-7xjl+ig&m$qfpf$_ehq!%#w@5y1o8@ahf&*r^5i-1=i^_%&@7k'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DEBUG', False)
 
 ALLOWED_HOSTS = ['8000-davidfb94-profitpt-wfk2lhjgkcn.ws.codeinstitute-ide.net']
 
