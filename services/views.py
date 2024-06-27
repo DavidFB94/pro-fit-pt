56cc3aea7181119715155f61c05ca58363231fe4
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-06-27 07:09:26 +0000

    Add pagination to services, change list name

--- services/views.py
+++ services/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator
 from .models import Service, PricingTier
+
 
 def all_services(request):
     services = Service.objects.all()
-    services_with_pricing = []
+    services_list = []
+    
+    paginator = Paginator(services, 5)
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
 
-    for service in services:
+    for service in page_obj:
         pricing_tier = PricingTier.objects.filter(service=service).first()
-        services_with_pricing.append((service, pricing_tier))
+        services_list.append((service, pricing_tier))
 
     context = {
-        'services_with_pricing': services_with_pricing,
+        'services_list': services_list,
+        "page_obj": page_obj,
     }
 
     return render(request, 'services/services.html', context)
