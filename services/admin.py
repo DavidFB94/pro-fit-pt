4e6f7d21b9f800fa05bc7e07541798885443710f
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-06-27 10:31:49 +0000

    Customize admin

--- services/admin.py
+++ services/admin.py
@@ -2,6 +2,26 @@ from django.contrib import admin
 from .models import Service, Category, PricingTier
 
 # Register your models here.
-admin.site.register(Service)
+class ServiceAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'category',
+        'description',
+    )
+
+    ordering = ('category',)
+
+
+class PricingTierAdmin(admin.ModelAdmin):
+    list_display = (
+        'service',
+        'quantity',
+        'price_per_unit',
+    )
+
+    ordering = ('service',)
+
+
+admin.site.register(Service, ServiceAdmin)
 admin.site.register(Category)
-admin.site.register(PricingTier)
+admin.site.register(PricingTier, PricingTierAdmin)
