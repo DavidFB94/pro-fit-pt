95978ffa51d8933fade160e43cf5c0706ec53f4f
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-06-27 15:31:28 +0000

    Add calculateTotalPrice js for service_details

---
+++ services/static/services/js/total_price.js
@@ -0,0 +1,13 @@
+function calculateTotalPrice() {
+    var select = document.getElementById("id_service_quantity");
+    if (!select.value || select.value === "Select amount") {
+        document.getElementById("total_price").innerText = "0.00 $";
+        return;
+    }
+    
+    var selectedOption = select.options[select.selectedIndex];
+    var quantity = parseInt(selectedOption.value);
+    var pricePerUnit = parseFloat(selectedOption.getAttribute("data-price"));
+    var totalPrice = quantity * pricePerUnit;
+    document.getElementById("total_price").innerText = totalPrice.toFixed(2) + " $";
+}
