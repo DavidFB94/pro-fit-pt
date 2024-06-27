8c18ddc8a3fdc4cdde91a98d7f1451018da53d9d
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-06-27 15:30:02 +0000

    Add service_details url

--- services/urls.py
+++ services/urls.py
@@ -1,7 +1,8 @@
-from django.contrib import admin
+from django.contrib import admin # do I need this here?
 from django.urls import path
 from . import views
 
 urlpatterns = [
     path('', views.all_services, name='services'),
+    path('<int:service_id>/', views.service_details, name='service_details'),
 ]
