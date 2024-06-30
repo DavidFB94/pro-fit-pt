45f8238fb506ba93084455e3d3e81cc0c7e5d7e4
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-06-30 13:01:07 +0000

    Add search functionality

--- services/views.py
+++ services/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
+from django.contrib import messages
+from django.db.models import Q
 from django.core.paginator import Paginator
 
 from .models import Service, PricingTier
@@ -8,6 +10,20 @@ def all_services(request):
     """ A view to show all services in a paginated list """
     # Fetch distinct services, ordered by name
     services = Service.objects.all().distinct().order_by('name')
+    query = None
+
+
+    # From CI Boutique Ado walkthrough
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('services'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query)
+            services = services.filter(queries)
+
 
     paginator = Paginator(services, 5)
     page_number = request.GET.get('page')
@@ -17,10 +33,12 @@ def all_services(request):
     for service in page_obj:
         pricing_tier = PricingTier.objects.filter(service=service).order_by('-price_per_unit').first()
         services_list.append((service, pricing_tier))
+    
 
     context = {
         'services_list': services_list,
         'page_obj': page_obj,
+        'search_term': query,
     }
 
     return render(request, 'services/services.html', context)
