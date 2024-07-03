a093f275ede666deedf98f91f818c41fee20844e
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-07-03 07:49:57 +0000

    Update context processor

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,8 @@
 from decimal import Decimal
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from services.models import Service, PricingTier
+
 def get_price_per_unit(service, quantity):
     """ Cart_contents helper function to access price_per_unit """
     pricing_tier = PricingTier.objects.filter(service=service, quantity__lte=quantity).order_by('-quantity').first()
@@ -10,14 +13,28 @@ def cart_contents(request):
 
     cart_items = []
     total = 0
-    product_count = 0
+    service_count = 0
+    cart = request.session.get('cart', {})
+
+    for item_id, quantity in cart.items():
+        service = get_object_or_404(Service, pk=item_id)
+        price_per_unit = get_price_per_unit(service, quantity)
+
+        total += quantity * price_per_unit
+        service_count += quantity
+
+        cart_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'service': service,
+        })
 
     grand_total = total
 
     context = {
         'cart_items': cart_items,
         'total': total,
-        'product_count': product_count,
+        'service_count': service_count,
         'grand_total': grand_total,
     }
 
