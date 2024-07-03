d300c9b3dfa5691ba432e61512822649c7674ef1
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-07-03 07:49:34 +0000

    Create helper function for cart_contents

--- cart/contexts.py
+++ cart/contexts.py
@@ -1,5 +1,10 @@
 from decimal import Decimal
 from django.conf import settings
+def get_price_per_unit(service, quantity):
+    """ Cart_contents helper function to access price_per_unit """
+    pricing_tier = PricingTier.objects.filter(service=service, quantity__lte=quantity).order_by('-quantity').first()
+    if pricing_tier:
+        return pricing_tier.price_per_unit
 
 def cart_contents(request):
 
