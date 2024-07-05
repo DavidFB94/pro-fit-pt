d9aaa27f36c9acdc44c85183b1a238168471408b
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-07-05 13:14:15 +0000

    Add notifications, cart preview and other messages

--- cart/views.py
+++ cart/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from services.models import Service
@@ -17,16 +17,17 @@ def add_to_cart(request, item_id):
     Add a quantity of the specified service to the shopping cart
     """
 
-    service = Service.objects.get(pk=item_id)
+    service = get_object_or_404(Service, pk=item_id)
     quantity = int(request.POST.get('service_quantity'))
     redirect_url = request.POST.get('redirect_url')
     cart = request.session.get('cart', {})
 
     if item_id in list(cart.keys()):
         cart[item_id] += quantity
+        messages.success(request, f'Updated {service.name} quantity to {cart[item_id]}')
     else:
         cart[item_id] = quantity
-        messages.success(request, f'Added {service.name} to your cart')
+        messages.success(request, f'Added {service.name} {quantity} to your cart')
     
     request.session['cart'] = cart
     return redirect(redirect_url)
@@ -37,12 +38,15 @@ def remove_from_cart(request, item_id):
     Remove the item from the shopping cart
     """
     try:
+        service = get_object_or_404(Service, pk=item_id)
         cart = request.session.get('cart', {})
 
         if item_id in cart:
             cart.pop(item_id)
+        messages.success(request, f'Removed {service.name} from your cart')
         
         request.session['cart'] = cart
         return HttpResponse(status=200)
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
