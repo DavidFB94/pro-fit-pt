1cf806832ce26410fa682a622a76ed226adeb359
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-07-09 12:22:04 +0000

    Update checkout models

--- checkout/models.py
+++ checkout/models.py
@@ -9,6 +9,7 @@ from services.models import Service, PricingTier
 
 class Order(models.Model):
     order_number = models.CharField(max_length=32, null=False, editable=False)
+    date = models.DateTimeField(auto_now_add=True)
     full_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
@@ -18,7 +19,6 @@ class Order(models.Model):
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
     county = models.CharField(max_length=80, null=True, blank=True)
-    date = models.DateTimeField(auto_now_add=True)
     grand_total = models.DecimalField(max_digits=10, decimal_places=2,
                                       null=False, default=0)
     original_bag = models.TextField(null=False, blank=False, default='')
@@ -55,13 +55,13 @@ class Order(models.Model):
 
 class OrderLineItem(models.Model):
     order = models.ForeignKey(Order, null=False, blank=False,
-                              on_delete=models.CASCADE,
-                              related_name='lineitems')
+                              on_delete=models.CASCADE, related_name='lineitems')
     service = models.ForeignKey(Service, null=False, blank=False,
                                 on_delete=models.CASCADE)
+    pricingtier = models.ForeignKey(PricingTier, null=True, blank=True,
+                                    on_delete=models.SET_NULL)
     lineitem_total = models.DecimalField(max_digits=6, decimal_places=2,
-                                         null=False, blank=False,
-                                         editable=False)
+                                         null=False, editable=False)
 
     def save(self, *args, **kwargs):
         """
