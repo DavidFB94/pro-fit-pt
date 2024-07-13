81377d32d44072ef44c58630b51a0eb123753237
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-07-13 10:51:36 +0000

    Remove quantity and price from service form

--- services/forms.py
+++ services/forms.py
@@ -1,34 +1,20 @@
 from django import forms
 from django.core.validators import MinValueValidator
+from django.forms import inlineformset_factory
 from .models import Service, Category, PricingTier
 
 
 class ServiceForm(forms.ModelForm):
-    quantity = forms.IntegerField(
-        required=True,
-        validators=[MinValueValidator(1)],
-    )
-    price_per_unit = forms.DecimalField(
-        max_digits=6,
-        decimal_places=2,
-        required=True,
-        validators=[MinValueValidator(0.01)],
-    )
-
     class Meta:
         model = Service
-        fields = ['name', 'category', 'description', 'quantity', 'price_per_unit', 'image',]
-        exclude = ['pricingtier_set']  # Exclude related PricingTier objects if there is a reverse relation
-
+        fields = ['name', 'category', 'description', 'image']
+    
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         categories = Category.objects.all()
         category_choices = [(c.id, c.name) for c in categories]
         self.fields['category'].choices = category_choices
+        
+        for field in self.fields.values():
+            field.widget.attrs['class'] = 'border-black rounded-0'
 
-        self.fields['quantity'].widget.attrs['class'] = 'border-black rounded-0'
-        self.fields['price_per_unit'].widget.attrs['class'] = 'border-black rounded-0'
-
-        for field_name, field in self.fields.items():
-            if field_name not in ['quantity', 'price_per_unit']:
-                field.widget.attrs['class'] = 'border-black rounded-0'
