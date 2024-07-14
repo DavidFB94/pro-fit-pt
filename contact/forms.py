7eaf1612d35e6453b128e9657e2af9ec2277ffc8
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-07-14 09:51:19 +0000

    Add ContactForm

---
+++ contact/forms.py
@@ -0,0 +1,25 @@
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ('name', 'email', 'message',)
+
+    def __init__(self, *args, **kwargs):
+        """
+        Add placeholders and classes, remove auto-generated
+        labels and set autofocus on first field
+        """
+        super().__init__(*args, **kwargs)
+        placeholders = {
+            'name': 'Name',
+            'email': 'Email address',
+            'message': 'Write your message here...',
+        }
+
+
+        for field in self.fields:
+            self.fields[field].widget.attrs['placeholder'] = placeholders[field]
+            self.fields[field].widget.attrs['class'] = 'form-control'
+            self.fields[field].label = False
