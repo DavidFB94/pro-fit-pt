dda7d4522afc46b9ab2a071f9f9f60a4ca820304
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-07-14 15:14:39 +0000

    Adjust contact view to save contact form

--- contact/views.py
+++ contact/views.py
@@ -1,13 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
 from .forms import ContactForm
 
 # Create your views here.
 
 def contact(request):
     """
-    Display the contact page
+    Display the contact page and save contact form
     """
-    form = ContactForm()
+    if request.method == 'POST':
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            form.save()
+            message.success(request, 'Your message has been sent!')
+            return redirect('contact')
+        else:
+            messages.error(request,
+                           ('Failed to send message. '
+                            'Please ensure the form is valid.'))
+    else:
+        form = ContactForm()
+    
     template = 'contact/contact.html'
     context = {
         'form': form,
