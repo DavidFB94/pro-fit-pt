b6c7dc7b670d789ab654108d580a40ae7fb8d980
David Flodin Bergqvist <david_flodin_bergqvist@live.se>
2024-07-21 09:19:54 +0000

    Create Newsletter model, add meta to Contact

--- about/models.py
+++ about/models.py
@@ -11,3 +11,13 @@ class FAQs(models.Model):
 
     def __str__(self):
         return self.question
+
+
+class Newsletter(models.Model):
+    class Meta:
+        verbose_name_plural = 'Newsletter list'
+
+    email = models.EmailField(max_length=254, null=False, blank=False)
+
+    def __str__(self):
+        return self.email
--- contact/models.py
+++ contact/models.py
@@ -3,6 +3,10 @@ from django.db import models
 # Create your models here.
 
 class Contact(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Customer messages'
+
     name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(max_length=254, null=False, blank=False)
     message = models.TextField(null=False, blank=False)
